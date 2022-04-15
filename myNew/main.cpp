dfce132c8dc8a2bb7399e631eaca62ead789e1cb
= <=>
2022-04-15 12:44:00 +0200

    Initial Commit

---
+++ myNew/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  myNew
+//
+//  Created by Valassi, Robert on 2022-04-15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
